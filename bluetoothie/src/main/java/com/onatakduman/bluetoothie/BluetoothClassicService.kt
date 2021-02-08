22d5f245f6f4c7d936593cd6e7b625adcaf9c4f5
Onat Akduman <onatakduman@gmail.com>
2021-02-08 20:29:43 +0300

    delay 5 ms between package s

--- bluetoothie/src/main/java/com/onatakduman/bluetoothie/BluetoothClassicService.kt
+++ bluetoothie/src/main/java/com/onatakduman/bluetoothie/BluetoothClassicService.kt
@@ -365,6 +365,7 @@ class BluetoothClassicService private constructor(bluetoothConfiguration: Blueto
                                     try {
                                         bufferedOutputStream.write(bufferC, 0, length)
                                         bufferedOutputStream.flush()
+                                        sleep(5)
                                     } catch (e: IOException) {
                                         sleep(200)
                                         e.printStackTrace()
